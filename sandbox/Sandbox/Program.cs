a3c64c3919e97a59f90c7d5731e1c80111a46ff5
Jethington <jethington22@gmail.com>
2023-09-16 14:27:48 -0600

    Testing and learning

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!!!");
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Your name is {name}");
+
+        if (name == "Jacob")
+        {
+            Console.WriteLine("Thats the name of who wrote this program");
+        
+        }
+
     }
 }
