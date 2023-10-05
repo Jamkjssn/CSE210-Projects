252b4af40a6810826691367fe591a5adb2ab0141
Jethington <jethington22@gmail.com>
2023-10-04 18:44:56 -0600

    Tested datetime

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,15 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your name? ");
-        string name = Console.ReadLine();
-        Console.WriteLine($"Your name is {name}");
-
-        if (name == "Jacob")
-        {
-            Console.WriteLine("Thats the name of who wrote this program");
-        
-        }
+        Console.WriteLine("Testing");
+        string Date = DateTime.Now.ToString("MM-dd-yy");
+        Console.WriteLine($"{Date}");
 
     }
 }
+
+
+// The shortcut for class creation is prop followed by tabs
