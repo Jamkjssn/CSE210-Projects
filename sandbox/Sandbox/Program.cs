655547d0eaf14b22343009bf400573bed2f2a5c8
Jethington <jethington22@gmail.com>
2023-11-19 00:08:41 -0600

    testing

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,12 +1,21 @@
 using System;
 using System.Threading;
 
-int time = 5;
-Console.Write("Countdown ... 5");
-Thread.Sleep(1000);
-for (int number = time-1; number >= 0; number--)
+// int time = 5;
+// Console.Write("Countdown ... 5");
+// Thread.Sleep(1000);
+// for (int number = time-1; number >= 0; number--)
+// {
+//     Console.Write($"\b{number}");
+//     Thread.Sleep(10);
+// }
+// Console.WriteLine("\nNice!");
+
+
+int _totalParts = 3;
+
+for (int i = 0; i <= _totalParts-1; i++)
 {
-    Console.Write($"\b{number}");
-    Thread.Sleep(1000);
+    Console.Write("[]");
 }
-Console.WriteLine("\nNice!");
+// Console.WriteLine("\t ratio?");
